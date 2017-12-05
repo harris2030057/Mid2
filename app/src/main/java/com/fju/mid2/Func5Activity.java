9e7015799a63cfb7e177316e42a8973c049694a3
Harris <harris2030057@gmail.com>
2017-12-05 11:35:38 +0800

    222

---
+++ app/src/main/java/com/fju/mid2/Func5Activity.java
@@ -0,0 +1,30 @@
+package com.fju.mid2;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Func5Activity extends AppCompatActivity {
+    public static final int FUNC_LOGIN = 1 ;
+    private final static  int REQUEST_LONGIN = 102;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_func5);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
--- app/src/main/java/com/fju/mid2/MainActivity.java
+++ app/src/main/java/com/fju/mid2/MainActivity.java
@@ -1,13 +1,101 @@
 package com.fju.mid2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final int FUNC_LOGIN = 1 ;
+    Boolean logon = false ;
+    private final static  int REQUEST_LONGIN = 102;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == FUNC_LOGIN){
+            if(resultCode == RESULT_OK){
+
+            }else{
+                finish();
+            }
+
+        }
+    }
+
+    public void func1(View view){
+        if(true){
+            Intent intent = new Intent(this , Func1Activity.class);
+           /* startActivity(intent);*/
+            startActivityForResult(intent , REQUEST_LONGIN);
+
+
+
+            finish();
+        }}
+    public void func2(View view){
+        if(true){
+
+            Intent intent = new Intent(this , Func2Activity.class);
+           /* startActivity(intent);*/
+            startActivityForResult(intent , REQUEST_LONGIN);
+            finish();
+        }}
+    public void func3(View view){
+        if(true){
+            Toast.makeText(this , "特殊功能", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(this , Func3Activity.class);
+           /* startActivity(intent);*/
+            startActivityForResult(intent , REQUEST_LONGIN);
+            finish();
+        }}
+    public void func4(View view){
+        if(true){
+
+            Intent intent = new Intent(this , Func4Activity.class);
+           /* startActivity(intent);*/
+            startActivityForResult(intent , REQUEST_LONGIN);
+            finish();
+        }}
+    public void func5(View view){
+        if(true){
+
+            Intent intent = new Intent(this , Func5Activity.class);
+           /* startActivity(intent);*/
+            startActivityForResult(intent , REQUEST_LONGIN);
+            finish();
+        }}
+    public void func6(View view){
+        if(true){
+
+            Intent intent = new Intent(this , Func6Activity.class);
+           /* startActivity(intent);*/
+            startActivityForResult(intent , REQUEST_LONGIN);
+            finish();
+        }}
+    public void func7(View view){
+        if(true){
+            Toast.makeText(this , "特殊功能", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(this , Func7Activity.class);
+           /* startActivity(intent);*/
+            startActivityForResult(intent , REQUEST_LONGIN);
+            finish();
+        }}
+    public void func8(View view){
+        if(true){
+            Toast.makeText(this , "特殊功能", Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(this , Func8Activity.class);
+           /* startActivity(intent);*/
+            startActivityForResult(intent , REQUEST_LONGIN);
+            finish();
+        }}
 }
