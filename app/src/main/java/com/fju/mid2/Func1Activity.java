ad1098f93aac7b34bc0dbd237ca1ed71ed89e1fe
hm5829 <hm5829@yahoo.com.tw>
2017-12-10 19:32:37 +0800

    change

--- app/src/main/java/com/fju/mid2/Func1Activity.java
+++ app/src/main/java/com/fju/mid2/Func1Activity.java
@@ -3,9 +3,12 @@ package com.fju.mid2;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+
+import java.util.Random;
 
 public class Func1Activity extends AppCompatActivity {
     public static final int FUNC_LOGIN = 1 ;
@@ -26,6 +29,13 @@ public class Func1Activity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+    public void num (View v){
+        Random r = new Random();
+        int i = r.nextInt(6) + 1;
+        new AlertDialog.Builder(this)
+                .setMessage("" + i)
+                .show();
     }
 
 }
